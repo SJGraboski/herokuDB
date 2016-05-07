bb57a6bee25399d4bf78723f240910526638df3e
Steven Graboski <sgrabosk@gmail.com>
2016-05-07 11:05:35 -0400

    changes to db files"

--- connection.js
+++ connection.js
@@ -1,11 +1,22 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-	database: "gh3skg4o4t43nk9s",
-	host: 'l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-	user: "k1mxo3c56hgag837",
-	password: "vok3g94mbi1b6vk8",
-	port: 3306
-});
+var source = {
+
+	localhost: {
+		port: 3306,
+		host: 'localhost', 
+		user: 'root',
+		password: '',
+		database:starwars
+	}
+	heroku: {
+		database: "gh3skg4o4t43nk9s",
+		host: 'l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+		user: "k1mxo3c56hgag837",
+		password: "vok3g94mbi1b6vk8",
+		port: 3306
+	}
+}
+var connection = mysql.createConnection(source.heroku);
 
 module.exports = connection;
