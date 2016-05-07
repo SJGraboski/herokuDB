042c41ed14a9fbbcf2f37461b990b03dbd645ebc
Steven Graboski <sgrabosk@gmail.com>
2016-05-07 10:43:01 -0400

    Changes to database files

--- connection.js
+++ connection.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-	database: "starwars_db",
-	host: 'localhost',
-	user: "root",
-	password: "",
+	database: "gh3skg4o4t43nk9s",
+	host: 'l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+	user: "k1mxo3c56hgag837",
+	password: "vok3g94mbi1b6vk8",
 	port: 3306
 });
 
